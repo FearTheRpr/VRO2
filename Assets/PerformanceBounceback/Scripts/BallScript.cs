db9a088752fdff344a73269a90821353af6b22d6
shanetomlinson <tiedupinside@gmail.com>
2020-12-04 09:01:08 -0700

    initial/final

---
+++ Assets/PerformanceBounceback/Scripts/BallScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour
+{
+
+
+    private void OnCollisionEnter(Collision col)
+    {
+
+        
+
+        if (col.gameObject.tag == "Trampoline") //if this object collides with another tagged "Trampoline"
+        {
+            gameObject.SetActive(false);
+           
+            
+        }
+    }
+}
---
+++ Assets/PerformanceBounceback/Scripts/BallSpawner.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallSpawner : MonoBehaviour {
+
+    public static BallSpawner current;
+
+    public GameObject pooledBall; //the prefab of the object in the object pool
+    public int amountOfBalls;
+    public List<GameObject> Balls;
+    public static int numberOfBalls;
+
+    void Awake()
+    {
+        current = this;
+    }
+    private void Start()
+    {
+        
+
+        Balls = new List<GameObject>();
+        for (int i = 0; i < amountOfBalls; i++)
+        {
+            GameObject ball = Instantiate(pooledBall);
+            ball.SetActive(false);
+            Balls.Add(ball);
+        }
+        InvokeRepeating("SpawnBall", 0f, 0.5f);
+    }
+    
+    public GameObject GetPooledObject()
+    {
+        for (int i = 0; 1 < Balls.Count; i++)
+        {
+            if (!Balls[i].activeInHierarchy)
+            {
+                return Balls[i];
+            } 
+        }
+        return null;
+    }
+    
+   	
+    void SpawnBall()
+    {
+        GameObject selectedBall = BallSpawner.current.GetPooledObject();
+
+        if (selectedBall != null)
+        {
+            selectedBall.SetActive(true);
+            Rigidbody selectedRigidBody = selectedBall.GetComponent<Rigidbody>();
+            selectedRigidBody.velocity = new Vector3(0, 0, 0 ); //resets ball velocity
+            selectedBall.transform.position = transform.position;
+        }
+
+        numberOfBalls++;
+        if (numberOfBalls >= amountOfBalls)
+        {
+            numberOfBalls = 0;
+        }
+    }
+	
+
+   
+    
+       
+    
+}
